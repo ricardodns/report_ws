dd90c38511f207c34e4c58ddb79589e103830024
Ricardo <ricardodnsousa@gmail.com>
2017-05-23 17:58:05 -0300

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-var route = require('./api/routes/reportRoute');
+const express = require('express')
+const app = express()
+var route = require('./api/routes/reportRoute')
 
 app.get('/', (req, res) => {
   res.send('HEY!')
-});
-app.listen(3000, () => console.log('Server running on port 3000'));
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
 
