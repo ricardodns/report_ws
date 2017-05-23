535616b11a06e53db6961a03d61335d6a49870bb
Ricardo <ricardodnsousa@gmail.com>
2017-05-23 20:27:17 -0300

    server.js update

--- outro/report_ws/routes/index.js
+++ outro/report_ws/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
-    Report = require('./api/models/reportModel'),
+    Report = require('api/models/reportModel'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var routes = require('./api/routes/reportRoute');
+var routes = require('api/routes/reportRoute');
 routes(app);
 
 
