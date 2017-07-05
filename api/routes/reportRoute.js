a8ec2fe2a005d59af51d762754c5a9428f5661a0
Ricardo <ricardodnsousa@gmail.com>
2017-07-05 15:39:54 -0300

    fix

--- api/routes/reportRoute.js
+++ api/routes/reportRoute.js
@@ -9,8 +9,7 @@ module.exports = function(app) {
     .post(reports.create_a_report);
 
 
-  app.route('/reports/:reportId')
-    .get(reports.read_a_report)
+  app.route('/reports/:reportId').get(reports.read_a_report)
     .put(reports.update_a_report)
     .delete(reports.delete_a_report);
 };
