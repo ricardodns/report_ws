b71a72c40b9818e7d012c38a67c9406256f38562
Ricardo <ricardodnsousa@gmail.com>
2017-07-20 22:12:58 -0300

    fix

--- server.js
+++ server.js
@@ -1,25 +1,56 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 3000,
-    mongoose = require('mongoose'),
-    Report = require('./api/models/reportModel.js'),
-    bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/server');
+var logger = require('morgan');
 
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var reports = require('./controllers/reportController.js');
+
+var app = express();
+app.use(logger('dev'));
 app.use(bodyParser.json());
-
-app.get('/test', function(req, res) { res.json({data : 'hello'});})
-
-var routes = require('./api/routes/reportRoute.js');
-routes(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
+//routes
+app.get('/', function(req, res){
+    res.jsonp({success: 'hello'});
+});
 
-app.listen(port);
+app.get('/reports', function(req, res){
+    res.jsonp(reports.list_all_reports);
+});
 
+app.listen(3000);
 
-console.log('Report RESTful API server started on: ' + port);
+console.log('Report RESTful API server started');
+
+// var express = require('express'),
+//     app = express(),
+//     port = process.env.PORT || 3000,
+//     mongoose = require('mongoose'),
+//     Report = require('./api/models/reportModel.js'),
+//     bodyParser = require('body-parser');
+//
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/server');
+//
+//
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+//
+// app.get('/test', function(req, res) { res.json({data : 'hello'});})
+//
+// var routes = require('./api/routes/reportRoute.js');
+// routes(app);
+//
+//
+//
+// app.listen(port);
+//
+//
+// console.log('Report RESTful API server started on: ' + port);
