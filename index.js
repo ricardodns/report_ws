b7dc02b51b814ae495be837970549613a66ce041
Ricardo <ricardodnsousa@gmail.com>
2017-07-20 22:28:18 -0300

    fix

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 var route = require('./api/routes/reportRoute');
 
 app.get('/', function(req, res) {
-  res.send('HEY!');
+  // res.send('HEY!');
+    res.redirect('./server.js');
 })
-app.listen(3000, function(){console.log('Server running on port 3000')});
+app.listen(3000, function(){console.log('lol - Server running on port 3000')});
 
