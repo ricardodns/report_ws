f24b98ab4fcb1689d9f3f9ecda33fbd48c80a210
Ricardo <ricardodnsousa@gmail.com>
2017-07-20 23:25:05 -0300

    fix

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var route = require('./api/routes/reportRoute');
 
 app.get('/', function(req, res) {
   // res.send('HEY!');
-    res.redirect('./server.js');
+    res.redirect('server.js');
 })
+
+app.get('/test', function(req, res) { res.json({data : 'hello'});})
 app.listen(3000, function(){console.log('lol - Server running on port 3000')});
 
